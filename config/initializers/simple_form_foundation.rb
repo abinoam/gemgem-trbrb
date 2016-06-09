7e7138fa918fcc72e2828b6feb460f5d8b70bb5c
Abinoam P. Marques Jr <abinoam@gmail.com>
2016-06-09 14:03:52 -0300

    Add a new SF wrapper so labels get rendered

--- config/initializers/simple_form_foundation.rb
+++ config/initializers/simple_form_foundation.rb
@@ -20,6 +20,15 @@ SimpleForm.setup do |config|
     # b.use :hint,  wrap_with: { tag: :span, class: :hint }
   end
 
+  config.wrappers :vertical_form_checkbox, class: :input, hint_class: :field_with_hint, error_class: :error do |b|
+    b.use :html5
+    b.optional :readonly
+    b.use :label_input
+    b.use :error, wrap_with: { tag: :small, class: :error }
+
+    # b.use :hint,  wrap_with: { tag: :span, class: :hint }
+  end
+
   config.wrappers :horizontal_form, tag: 'div', class: 'row', hint_class: :field_with_hint, error_class: :error do |b|
     b.use :html5
     b.use :placeholder
